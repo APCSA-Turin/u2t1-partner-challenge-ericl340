a4bb6a2122e49b35066494a2d43ead8048dda4a9
ericl340 <ericl340@nycstudents.net>
2024-09-25 12:48:39 +0000

    update

--- src/main/java/com/example/project/CalculatorRunner.java
+++ src/main/java/com/example/project/CalculatorRunner.java
@@ -1,12 +1,16 @@
 package com.example.project;
 
+
 public class CalculatorRunner {
     public static void main(String[] args) {
-
-        //test your program here
-        //1. create an instance of the calculator class
-
-        //2. call any methods of that class to test
-
+        Calculator calc1 = new Calculator("TI84");
+        System.out.println(calc1.coordinatePair(5, 6));
+        System.out.println(calc1.performOperation("+", 6, 4));
+        System.out.println(calc1.performOperation("-", 6, 4));
+        System.out.println(calc1.performOperation("/", 6, 4));
+        System.out.println(calc1.performOperation("*", 6, 4));
+        System.out.println(calc1.divisibleBy(124562, 4));
+        System.out.println(calc1.absoluteValue(564412, 2414352));
+        System.out.println(calc1.info());
     }
 }
